c19e4dbda2fb23f88f543242c2d2e6fc3bff3e83
naufinur <khayatinaufi23gmail.com>
2024-07-25 20:19:57 +0700

    ⚙:fix cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://besthy-be.vercel.app",
   })
 );
 app.use(
@@ -36,7 +36,7 @@ app.use("/detak", detakRoute);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://besthy-be.vercel.app",
   },
 });
 
